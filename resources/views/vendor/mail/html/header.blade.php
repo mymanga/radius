df513559215fdcfb88c7adfd272ad55cbe480bc9
Kelvin M <kelvmuriuki@icloud.com>
2023-09-01 13:24:13 +0300

    deleted

--- resources/views/vendor/mail/html/header.blade.php
+++
@@ -1,5 +0,0 @@
-<tr>
-<td class="header">
-{{-- <img src="{{ asset(setting('logo')) }}" class="logo" alt="{{ setting('company') }}"> --}}
-</td>
-</tr>
--- vendor/evilfreelancer/openvpn-php/src/Types/Push.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace OpenVPN\Types;
-
-class Push
-{
-
-}
