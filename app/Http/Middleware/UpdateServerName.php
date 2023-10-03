56137a8a02b43d6f891b1d6b49e5c92cf4981c62
Kelvin M <kelvmuriuki@icloud.com>
2023-10-04 00:04:12 +0300

    v1.3.6

--- app/Http/Middleware/UpdateServerName.php
+++ app/Http/Middleware/UpdateServerName.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use App\Models\Setting;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 
 class UpdateServerName
 {
@@ -20,28 +21,31 @@ class UpdateServerName
     {
         $serverName = $request->getHttpHost();
 
-        // Retrieve the server name from the cache
-        $cachedServerName = Cache::get('server_name');
+        // Check if the 'radius' database connection is configured and the 'settings' table exists
+        if (config('database.connections.radius') && Schema::connection('radius')->hasTable('settings')) {
+            // Retrieve the server name from the cache
+            $cachedServerName = Cache::get('server_name');
 
-        // If the server name is not in the cache or has changed, update the database and the cache
-        if ($cachedServerName !== $serverName) {
-            
-            // Get or create a setting instance with the key 'server_name'
-            setting(['server_name' => $serverName])->save();
-            
-            // Update the cache with a new expiration time of 24 hours
-            Cache::put('server_name', $serverName, now()->addHours(24));
+            // If the server name is not in the cache or has changed, update the database and the cache
+            if ($cachedServerName !== $serverName) {
+                
+                // Get or create a setting instance with the key 'server_name'
+                setting(['server_name' => $serverName])->save();
+                
+                // Update the cache with a new expiration time of 24 hours
+                Cache::put('server_name', $serverName, now()->addHours(24));
+            }
+
+            // Handle the update check and cache the data for 24 hours
+            $api = new \LicenseBoxExternalAPI();
+
+            $updateData = Cache::remember('update_data', 60 * 24, function () use ($api) {
+                return $api->check_update();
+            });
+
+            // Share the update data with all your views
+            view()->share('updateData', $updateData);
         }
-
-        // Handle the update check and cache the data for 24 hours
-        $api = new \LicenseBoxExternalAPI();
-
-        $updateData = Cache::remember('update_data', 60 * 24, function () use ($api) {
-            return $api->check_update();
-        });
-
-        // Share the update data with all your views
-        view()->share('updateData', $updateData);
 
         return $next($request);
     }
--- app/Http/ViewComposers/SupportComposer.php
+++ app/Http/ViewComposers/SupportComposer.php
@@ -3,13 +3,29 @@
 namespace App\Http\ViewComposers;
 
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 use App\Models\EmailConversation;
 
 class SupportComposer
 {
     public function compose(View $view)
     {
-        // $unreadCount = EmailConversation::where('is_read', false)->count();
-        // $view->with('unreadCount', $unreadCount);
+        // Check if the 'radius' database connection is configured
+        if (config('database.connections.radius')) {
+            $databaseName = config('database.connections.radius.database');
+
+            // Check if the 'email_conversations' table exists in the 'radius' database
+            if (Schema::connection('radius')->hasTable('email_conversations')) {
+                $unreadCount = EmailConversation::where('is_read', false)->count();
+                $view->with('unreadCount', $unreadCount);
+            } else {
+                // Handle the case when the 'email_conversations' table does not exist in 'radius'
+                $view->with('unreadCount', 0); // You can set a default value or handle it as needed
+            }
+        } else {
+            // Handle the case when the 'radius' database connection is not configured
+            $view->with('unreadCount', 0); // You can set a default value or handle it as needed
+        }
     }
 }
